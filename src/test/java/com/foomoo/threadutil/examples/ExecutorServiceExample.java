fb3f6fafb3798d8208086f7604bf26644eaab769
Daniel Watford <dan@foomoo.com>
2017-09-18 20:53:42 +0100

    Added an example showing use of the ContextCopyingThreadFactory with an ExecutorService.

---
+++ src/test/java/com/foomoo/threadutil/examples/ExecutorServiceExample.java
@@ -0,0 +1,58 @@
+package com.foomoo.threadutil.examples;
+
+import com.foomoo.threadutil.ContextCopyingThreadFactory;
+import com.google.common.collect.ImmutableList;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ExecutorServiceExample {
+
+    /**
+     * Example use case of the {@link ContextCopyingThreadFactory} where the RequestId for a server processed request is stored in thread-specific
+     * storage and copied to other threads.
+     * <p>
+     * In this example the original request id is set, the executor created and then the request id changed. The {@link Runnable} submitted to the
+     * executor then log output to demonstrate the RequestId values held in their thread specific storage.
+     *
+     * @throws InterruptedException Not expected to be thrown.
+     */
+    @Test
+    public void example() throws InterruptedException {
+
+        XRequestIdStorage.setRequestId("OriginalRequestId");
+
+        final Logger logger = LogManager.getLogger();
+        logger.info("Example start");
+
+        final ContextCopyingThreadFactory threadFactory = new ContextCopyingThreadFactory(ImmutableList.of(new XRequestIdContextCopier()));
+        final ExecutorService executorService = Executors.newFixedThreadPool(5, threadFactory);
+
+        XRequestIdStorage.setRequestId("NewRequestId");
+
+        executorService.submit(getRunnable(1, logger));
+        executorService.submit(getRunnable(2, logger));
+        executorService.submit(getRunnable(3, logger));
+
+        logger.info("All tasks submitted");
+
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.SECONDS);
+    }
+
+    private Runnable getRunnable(final int taskId, final Logger logger) {
+        return () -> {
+            logger.info("Task {} before sleep", taskId);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            logger.info("Task {} after sleep", taskId);
+        };
+    }
+}
