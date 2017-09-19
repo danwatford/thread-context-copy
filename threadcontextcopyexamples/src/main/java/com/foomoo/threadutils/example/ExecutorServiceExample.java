7f10d123965ddae11b72a57f9ed704156abf2a03
Daniel Watford <dan@foomoo.com>
2017-09-19 18:26:00 +0100

    Cleaned up XRequestId example to be contained in a single class file.

--- threadcontextcopyexamples/src/main/java/com/foomoo/threadutils/example/ExecutorServiceExample.java
+++ threadcontextcopyexamples/src/main/java/com/foomoo/threadutils/example/ExecutorServiceExample.java
@@ -1,28 +1,37 @@
 package com.foomoo.threadutils.example;
 
+import com.foomoo.threadutil.ContextCopier;
 import com.foomoo.threadutil.ContextCopyingThreadFactory;
 import com.google.common.collect.ImmutableList;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
 
+import java.util.Collections;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 public class ExecutorServiceExample {
+
+    private static final String KEY = "requestId";
 
     /**
      * Example use case of the {@link ContextCopyingThreadFactory} where the RequestId for a server processed request is stored in thread-specific
      * storage and copied to other threads.
      * <p>
-     * In this example the original request id is set, the executor created and then the request id changed. The {@link Runnable} submitted to the
-     * executor then log output to demonstrate the RequestId values held in their thread specific storage.
+     * In this example the original request id is set and then the executor is created, making use of the {@link ContextCopyingThreadFactory} itself
+     * configured with an instance of {@link XRequestIdContextCopier}. The {@link Callable}s submitted to the executor then log output to demonstrate
+     * the RequestId values held in their thread specific storage.
      *
      * @throws InterruptedException Not expected to be thrown.
      */
     public static void main(final String args[]) throws InterruptedException {
 
-        XRequestIdStorage.setRequestId("OriginalRequestId");
+        setRequestId("000");
 
         final Logger logger = LogManager.getLogger();
         logger.info("Example start");
@@ -30,11 +39,8 @@ public class ExecutorServiceExample {
         final ContextCopyingThreadFactory threadFactory = new ContextCopyingThreadFactory(ImmutableList.of(new XRequestIdContextCopier()));
         final ExecutorService executorService = Executors.newFixedThreadPool(5, threadFactory);
 
-        XRequestIdStorage.setRequestId("NewRequestId");
-
-        executorService.submit(getRunnable(1, logger));
-        executorService.submit(getRunnable(2, logger));
-        executorService.submit(getRunnable(3, logger));
+        IntStream.rangeClosed(1, 20)
+                 .forEach(taskId -> executorService.submit(getRunnable(taskId, logger)));
 
         logger.info("All tasks submitted");
 
@@ -42,15 +48,42 @@ public class ExecutorServiceExample {
         executorService.awaitTermination(1, TimeUnit.SECONDS);
     }
 
-    private static Runnable getRunnable(final int taskId, final Logger logger) {
+    private static Callable<Void> getRunnable(final int taskId, final Logger logger) {
         return () -> {
-            logger.info("Task {} before sleep", taskId);
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            logger.info("Task {} after sleep", taskId);
+            final String padding = String.join("", Collections.nCopies(taskId, "  "));
+            final String message = String.format("%s%02d", padding, taskId);
+            logger.info(message);
+            Thread.sleep(200);
+            logger.info(message);
+            return null;
         };
+    }
+
+    static void setRequestId(final String requestId) {
+        ThreadContext.put(KEY, requestId);
+    }
+
+    static String getRequestId() {
+        return ThreadContext.get(KEY);
+    }
+
+    /**
+     * {@link com.foomoo.threadutil.ContextCopier} for interacting with the X Request Id thread-specific storage in order to copy X Request Id values to
+     * new threads. Applies a suffix to the requestId for each thread created.
+     */
+    private static class XRequestIdContextCopier implements ContextCopier {
+
+        private String requestId;
+        private AtomicInteger applyCount = new AtomicInteger();
+
+        @Override
+        public void copy() {
+            requestId = getRequestId();
+        }
+
+        @Override
+        public void apply() {
+            setRequestId(String.format("%s-%02d", requestId, applyCount.getAndIncrement()));
+        }
     }
 }
